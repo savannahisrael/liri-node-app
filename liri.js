96ef6e589f1f4b5a4b13883d71707bbc706ec7cc
Savannah Israel <sisrael0405@gmail.com>
2019-06-26 12:36:13 -0600

    concert and movie functions working

---
+++ liri.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const axios = require('axios');
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+// 1557ce71 OMDB API KEY
+var action = process.argv[2].toLowerCase();
+var searchMovie = process.argv.slice(3).join("+");
+var searchConcert = process.argv.slice(3).join("%20");
+var display = process.argv.slice(3).join(" ");
+
+// function spotifySearch() {
+//    console.log("This is the spotify search");
+// };
+
+function omdbSearch() {
+    axios.get("http://www.omdbapi.com/?apikey=1557ce71&t=" + searchMovie)
+        .then(function (response) {
+            console.log(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB Rating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.dataLanguage}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n\n================================================\n\n`);
+ 
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    console.log(`\n\n========== OMDB RESULTS FOR: ${display} ==========\n`);
+ };
+
+function concertSearch() {
+   console.log(`\n\n========== UPCOMING CONCERTS FOR: ${display} ==========\n\n`);
+   axios.get("https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp").then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+        console.log(`----------\nVenue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${response.data[i].datetime}\n----------\n\n`);
+    }; 
+   });
+};
+
+function doWhatItSays() {
+   console.log("This is Do what it says");
+};
+
+switch(action) {
+//    case "spotify-this-song":
+//        spotifySearch();
+//        break;
+   case "movie-this":
+       omdbSearch();
+       break;
+   case "concert-this":
+       concertSearch();
+       break;
+   case "do-what-it-says":
+       doWhatItSays();
+       break;
+   default:
+       return console.log("Choose a valid action");
+}
+
+
